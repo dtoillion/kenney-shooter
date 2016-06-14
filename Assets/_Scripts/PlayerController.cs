9a0fedc0f494cbba1f4824ff7b80500b6d7b5d5c
dtoillion <toillion@gmail.com>
2016-06-14 01:30:21 -0700

    twin stick controls

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -3,10 +3,6 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour 
 {
-	public GameObject ForwardThrust;
-	public GameObject BackwardThrust;
-	public GameObject LeftThrust;
-	public GameObject RightThrust;
 	public GameObject LaserPrefab;
 	public GameObject ExplosionPrefab;
 	public GameObject ImpactPrefab;
@@ -14,20 +10,19 @@ public class PlayerController : MonoBehaviour
 	private float YInputValue;
 	private float XInputValue;
 	private Rigidbody2D rb;
-
-	AudioSource audio;
+	private bool crash;
 
 	private void Awake ()
 	{
 		rb = GetComponent<Rigidbody2D>();
-		audio = GetComponent<AudioSource>();
-		ThrustControl();
+		crash = false;
 	}
 
 	private void Update ()
 	{
 		YInputValue = Input.GetAxis("Vertical");
 		XInputValue = Input.GetAxis("Horizontal");
+
 		if(GameControl.control.health <= 0)
 		{
 			Instantiate(ExplosionPrefab, transform.position, transform.rotation);
@@ -38,18 +33,13 @@ public class PlayerController : MonoBehaviour
 	
 	private void FixedUpdate ()
 	{
-		MoveShip ();
-		TurnShip ();
+		if(((XInputValue != 0) || (YInputValue != 0)) && !(crash))
+			TurnShip ();
 		
-		if((YInputValue != 0f) || (XInputValue != 0f))
-		{
-			ThrustControl ();
-			audio.pitch = 1f;
-			} else {
-				audio.pitch = 0.3f;
-		}
-
-		if(Input.GetButton("Fire1") && Time.time > nextFire)
+		if(Input.GetButton("Fire1"))
+			MoveShip ();
+		
+		if(Input.GetButton("Fire2") && Time.time > nextFire)
 		{
 			nextFire = Time.time + GameControl.control.fireRate;
 			Instantiate(LaserPrefab, transform.position, transform.rotation);
@@ -67,40 +57,29 @@ public class PlayerController : MonoBehaviour
 	
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		if (collision.gameObject.tag == "Target")
+		if ((collision.gameObject.tag == "Target") && !(crash))
 		{
+			crash = true;
+			rb.AddTorque(10);
+			Invoke("Crashed", 2);
 			GameControl.control.health-=10f;
 			Instantiate(ImpactPrefab, transform.position, transform.rotation);
 		}
 	}
 
+	private void Crashed ()
+	{
+		crash = false;
+	}
+
 	private void MoveShip ()
 	{
-		rb.AddForce(transform.up * YInputValue * GameControl.control.Speed * Time.deltaTime);
+		rb.AddForce(transform.up * GameControl.control.Speed);
 	}
 
 	private void TurnShip ()
 	{
-		rb.AddForce(transform.right * XInputValue * GameControl.control.Speed * Time.deltaTime);
-	}
-
-	private void ThrustControl ()
-	{
-		if(YInputValue == 1) {
-			ForwardThrust.SetActive(true);
-		} else if(YInputValue == -1) {
-			BackwardThrust.SetActive(true);
-		} else {
-			ForwardThrust.SetActive(false);
-			BackwardThrust.SetActive(false);
-		}
-		if(XInputValue == 1) {
-			RightThrust.SetActive(true);
-		} else if(XInputValue == -1) {
-			LeftThrust.SetActive(true);
-		} else {
-			RightThrust.SetActive(false);
-			LeftThrust.SetActive(false);
-		}
+		var angle = Mathf.Atan2(XInputValue, YInputValue) * Mathf.Rad2Deg - 180;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
 	}
 }
