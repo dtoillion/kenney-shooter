45ac8620f99a81f4527e158ed84d073ba932579b
dtoillion <toillion@gmail.com>
2016-06-23 22:07:12 -0700

    resolution fix for newgrounds player

--- Assets/_Scripts/DestroyOnTouch.cs
+++ Assets/_Scripts/DestroyOnTouch.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class DestroyOnTouch : MonoBehaviour {
 
+  public bool laserTrap = false;
+  public bool destroyAll = true;
+
 	void OnTriggerEnter2D(Collider2D trig)
 	{
-    Destroy(trig.gameObject, 0);
+    if(destroyAll)
+      Destroy(trig.gameObject, 0);
+    if((laserTrap) && (trig.gameObject.tag == "Projectile"))
+      Destroy(trig.gameObject, 0);
   }
 
 }
